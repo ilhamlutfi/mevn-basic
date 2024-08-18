9f37c338eab601e2347cba3216dcafd8588c80ad
ilhamlutfi <ilhamlutfi153@gmail.com>
2024-08-18 18:36:11 +0700

    first commit

---
+++ express-api/index.js
@@ -0,0 +1,26 @@
+// import module
+const express = require('express')
+const cors = require('cors')
+const parser = require('body-parser')
+
+// init app
+const app = express()
+
+// use cors
+app.use(cors())
+
+// use body parser
+app.use(parser.urlencoded({ extended: false }))
+app.use(parser.json())
+
+// define port
+const port = 3000
+
+// define routes
+app.get('/', (req, res) => {
+    res.send('Hello World!')})
+
+// start server
+app.listen(port, () => {
+    console.log(`Server started on port ${port}`)
+})
