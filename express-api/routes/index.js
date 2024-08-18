568c1f23158f1b00140f4c23b6224bf054224324
ilhamlutfi <ilhamlutfi153@gmail.com>
2024-08-18 20:39:01 +0700

    add controller api login

--- express-api/routes/index.js
+++ express-api/routes/index.js
@@ -1,8 +1,11 @@
 const router = require('express').Router()
-const { validateRegister } = require('../helper/validators/auth')
+const { validateRegister, validateLogin } = require('../helper/validators/auth')
 
 const RegisterController = require('../controllers/RegisterController')
+const LoginController = require('../controllers/LoginController')
 
 router.post('/register', validateRegister, RegisterController.store)
+
+router.post('/login', validateLogin, LoginController.submit)
 
 module.exports = router
