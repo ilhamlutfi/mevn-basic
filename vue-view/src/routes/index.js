ddf8079612a6751eff4f8480c31e4ff3be8b36b3
ilhamlutfi <ilhamlutfi153@gmail.com>
2024-08-19 13:04:33 +0700

    add function guard route + dashboard view

--- vue-view/src/routes/index.js
+++ vue-view/src/routes/index.js
@@ -1,11 +1,22 @@
 // import vue router
 import { createRouter, createWebHistory } from 'vue-router'
+import Cookie from 'js-cookie'
+
+const getToken = () => {
+    return Cookie.get('token')
+}
 
 const routes = [
     {
         path: '/',
         name: 'home',
         component: () => import('../views/home/index.vue')
+    },
+    {
+        path: '/',
+        name: 'dashboard',
+        component: () => import('../views/dashboard/index.vue'),
+        meta: { requiresAuth: true }
     },
     {
         path: '/register',
@@ -22,6 +33,24 @@ const routes = [
 const router = createRouter({
     history: createWebHistory(),
     routes
+})
+
+// guard navigation
+router.beforeEach((to, from, next) => {
+    // get token user
+    const token = getToken()
+
+    // Jika rute tujuan membutuhkan otentikasi dan pengguna tidak memiliki token
+    if (to.matched.some(record => record.meta.requiresAuth) && !token) {
+        next({ name: 'login' })        
+    }
+    // Jika rute tujuan adalah halaman login atau register tapi pengguna sudah login
+    else if ((to.name === 'login' || to.name === 'register') && token) {
+        next({ name: 'dashboard'})
+    }
+    else {
+        next()
+    }
 })
 
 export default router
